036710f834fa27e31e2cdea710029db2dc7e660a
asaka <aisk1988@gmail.com>
2014-08-16 19:21:41 +0800

    add allowd hosts

--- hosts.go
+++ hosts.go
@@ -1,14 +1,41 @@
 package main
 
 var allowdHosts = map[string]bool{
-	"zhinsta.com:8080": true,
-	"amazonaws.com":    true,
-	"fbcdn.net":        true,
-	"instagram.com":    true,
-	"cdninstagram.com": true,
+	"distillery.s3.amazonaws.com":        true,
+	"distilleryimage0.s3.amazonaws.com":  true,
+	"distilleryimage1.s3.amazonaws.com":  true,
+	"distilleryimage2.s3.amazonaws.com":  true,
+	"distilleryimage3.s3.amazonaws.com":  true,
+	"distilleryimage4.s3.amazonaws.com":  true,
+	"distilleryimage5.s3.amazonaws.com":  true,
+	"distilleryimage6.s3.amazonaws.com":  true,
+	"distilleryimage7.s3.amazonaws.com":  true,
+	"distilleryimage8.s3.amazonaws.com":  true,
+	"distilleryimage9.s3.amazonaws.com":  true,
+	"distilleryimage10.s3.amazonaws.com": true,
+	"distilleryimage11.s3.amazonaws.com": true,
+	"origincache-ash.fbcdn.net":          true,
+	"origincache-frc.fbcdn.net":          true,
+	"origincache-prn.fbcdn.net":          true,
+	"photos-a.ak.instagram.com":          true,
+	"photos-b.ak.instagram.com":          true,
+	"photos-c.ak.instagram.com":          true,
+	"photos-d.ak.instagram.com":          true,
+	"photos-e.ak.instagram.com":          true,
+	"photos-f.ak.instagram.com":          true,
+	"photos-g.ak.instagram.com":          true,
+	"photos-h.ak.instagram.com":          true,
+	"scontent-a.cdninstagram.com":        true,
+	"scontent-b.cdninstagram.com":        true,
+	"scontent-c.cdninstagram.com":        true,
+	"scontent-d.cdninstagram.com":        true,
+	"scontent-e.cdninstagram.com":        true,
+	"scontent-f.cdninstagram.com":        true,
+	// "zhinsta.com:8080":                   true, // for debug
 }
 
 func isHostAllowed(host string) bool {
+	println(host)
 	_, ok := allowdHosts[host]
 	return ok
 }
